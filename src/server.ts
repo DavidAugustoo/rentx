860a0c5b9e4fbf727c0848e55adad5c9afef86d9
David Augusto <davidribeiro086@gmail.com>
2022-04-23 19:01:32 -0300

    :wrench: chore: configuring the debbuger

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (resquest, response) => {
     return response.json("HEllo World!");
 });
 
-app.listen(3333, () => console.log("Server is running!"));
+app.post("/", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ name });
+});
+
+app.listen(3000, () => console.log("Server is running!"));
