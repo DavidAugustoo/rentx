f9a12d5adf61abaa10a985b7f72295fdeb60f188
David Augusto <davidribeiro086@gmail.com>
2022-06-03 18:11:20 -0300

    :feat: specification route creation

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,7 +1,26 @@
-class Specication {
-    execute() {
-        console.log("teste");
+import { ISpecificationRepository } from "../repositories/ISpecificationRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    constructor(private specificationsRepository: ISpecificationRepository) {}
+
+    execute({ name, description }: IRequest) {
+        const specificationAlreadyExistis =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExistis) {
+            throw new Error("Specification Already Exists");
+        }
+
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
     }
 }
 
-export { Specication };
+export { CreateSpecificationService };
